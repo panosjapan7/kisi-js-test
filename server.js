003750ec348a177836a03a199f63b9fa780f12ac
panosjapan7 <panosjapan@gmail.com>
2022-07-11 08:17:02 +0200

    GET data from API with axios

--- server.js
+++ server.js
@@ -5,10 +5,24 @@ const router = express.Router;
 const axios = require("axios");
 const PORT = 3000;
 
+// View engine setup
 app.set("view engine", "html");
 
 
+let apiData;
+// GET data from API - Axios
+axios
+    .get('https://api.themoviedb.org/3/trending/all/day?api_key=238cb64db6302e7587f0099a49af4b3d&page=1')
+    .then(res => {
+        console.log("Status Code: ", res.status);
+        apiData = res.data.results;
+        console.log(apiData);
+    })
+    .catch(error => {
+        console.log(error);
+    })
 
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
